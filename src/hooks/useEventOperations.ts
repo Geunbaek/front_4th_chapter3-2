bf3a014b92620ab1ac7f36ea41cbadfa1202a79f
Geunbaek <rmsqor103@naver.com>
2025-02-14 04:26:00 +0900

    feat: useEventOperations 반복 이벤트 관련 로직 추가

--- src/hooks/useEventOperations.ts
+++ src/hooks/useEventOperations.ts
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 
 import * as apis from '../apis/events';
 import { Event, EventForm } from '../types';
+import { generateRepeatDates } from '../utils/dateRepeat';
 
 export const useEventOperations = (editing: boolean, onSave?: () => void) => {
   const [events, setEvents] = useState<Event[]>([]);
@@ -23,12 +24,52 @@ export const useEventOperations = (editing: boolean, onSave?: () => void) => {
     }
   };
 
-  const saveEvent = async (eventData: Event | EventForm) => {
+  const saveEvent = async (
+    eventData: Event | EventForm,
+    repeatEditMode: 'single' | 'all' = 'single'
+  ) => {
+    const isRepeatEvent = !!eventData.repeat.id;
     try {
       if (editing) {
-        await apis.updateEvent(eventData as Event);
+        // 이벤트 수정
+        if (isRepeatEvent) {
+          // 기존 반복 이벤트
+          // -> repeatEditMode: all 같은 반복이벤트 모두 수정
+          // -> repeatEditMode: single 현재 이벤트만 삭제 후 단일 이벤트로 수정
+          if (eventData.repeat.type === 'none') return;
+          if (repeatEditMode === 'all') {
+            const repeatEvents = events
+              .filter((event) => event.repeat.id === eventData.repeat.id)
+              .map((event) => ({
+                ...eventData,
+                id: event.id,
+                date: event.date,
+              }));
+            await apis.updateEventsList(repeatEvents);
+          } else {
+            await apis.deleteEvent((eventData as Event).id);
+            await apis.addEvent({ ...eventData, repeat: { ...eventData.repeat, id: undefined } });
+          }
+        } else {
+          await apis.updateEvent(eventData as Event);
+        }
       } else {
-        await apis.addEvent(eventData as EventForm);
+        // 이벤트 추가
+        // 기본 일정 추가: addEvent 호출
+        // 반복 일정 추가: addEventList 호출
+        if (eventData.repeat.type === 'none') {
+          await apis.addEvent(eventData as EventForm);
+        } else {
+          const dates = generateRepeatDates({
+            startDate: eventData.date,
+            endDate: eventData.repeat.endDate,
+            frequency: eventData.repeat.type,
+            interval: eventData.repeat.interval,
+            occurrences: eventData.repeat.count,
+          });
+          const repeatEvents = dates.map((date) => ({ ...eventData, date }));
+          await apis.addEventsList(repeatEvents);
+        }
       }
 
       await fetchEvents();
@@ -50,9 +91,13 @@ export const useEventOperations = (editing: boolean, onSave?: () => void) => {
     }
   };
 
-  const deleteEvent = async (id: string) => {
+  const deleteEvent = async (id: string | string[]) => {
     try {
-      await apis.deleteEvent(id);
+      if (!Array.isArray(id)) {
+        await apis.deleteEvent(id);
+      } else {
+        await apis.deleteEventsList(id);
+      }
       await fetchEvents();
       toast({
         title: '일정이 삭제되었습니다.',
